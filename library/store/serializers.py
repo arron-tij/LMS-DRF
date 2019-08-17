c4d20b919c9d79611d56f440bd1f32cefd581ec7
Amritansh Singhal <amritansh.singhal.cse17@itbhu.ac.in>
2019-08-18 01:31:56 +0530

    Used serializers for Book Order

--- library/store/serializers.py
+++ library/store/serializers.py
@@ -19,3 +19,16 @@ class BookOrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = BookOrder
         fields = ('id', 'books', 'customer', )
+
+class OrderBookSerializer(serializers.Serializer):
+    book = serializers.PrimaryKeyRelatedField(queryset=Book.objects.all())
+
+
+    def save(self):
+        book = self.validated_data['book']
+        user=self.context['request'].user
+        order, created = BookOrder.objects.get_or_create(customer=user)
+        if order.books.filter(id=book.id):
+            raise serializers.ValidationError("Book already loaned")
+        order.books.add(book)
+        return order
--- library/store/views.py
+++ library/store/views.py
@@ -1,26 +1,24 @@
-from rest_framework import generics
+from rest_framework import generics,authentication,permissions,status
 from rest_framework.response import Response
 from .models import *
-from .serializers import BookSerializer, BookDetailSerializer, BookOrderSerializer
+from .serializers import BookSerializer, BookDetailSerializer, BookOrderSerializer, OrderBookSerializer
 from django.http import HttpResponse
 class BookList(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
 class OrderBook(generics.GenericAPIView):
+    permission_classes=[permissions.IsAuthenticated]
     queryset = Book.objects.all()
-    serializer_class = BookSerializer
+    serializer_class = OrderBookSerializer
 
-    def get(self,request,pk):
-        serializer = self.queryset.get(pk=pk)
-        try:
-            instnc=BookOrder.objects.get(customer=request.user.id)
-            BookOrder.objects.get(id=instnc.id).books.add(serializer)
-        except:
-            BookOrder.objects.create(customer=request.user)
-            instnc=BookOrder.objects.get(customer=request.user.id)
-            BookOrder.objects.get(id=instnc.id).books.add(serializer)
-        return Response()
+    def post(self, request, *args, **kwargs):
+        self.request = request
+        self.serializer = self.get_serializer(data = request.data)
+        self.serializer.is_valid(raise_exception=True)
+        order = self.serializer.save()
+        return Response(status = status.HTTP_200_OK)
+    
 
 class ReturnBook(generics.GenericAPIView):
     queryset = Book.objects.all()
